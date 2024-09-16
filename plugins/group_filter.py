6085a85cdd782f8d7c71ae39917325729fcdd570
Dinidusilva66 <118092626+Dinidusilva66@users.noreply.github.com>
2024-09-16 19:19:08 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -100,7 +100,7 @@ async def next_page(bot, query):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
 
-    btn.insert(0, [InlineKeyboardButton("🌼 Jᴏɪɴ Fɪʟᴍ Cʜᴀɴɴᴇʟ 🌼", url="https://t.me/SilvaFilmZone")])
+    btn.insert(0, [InlineKeyboardButton("🌼 Jᴏɪɴ Fɪʟᴍ Cʜᴀɴɴᴇʟ 🌼", url="https://t.me/SinhalaSubMovies")])
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
@@ -216,7 +216,7 @@ async def auto_filter(client, msg, spoll=False):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'{pre}#{req}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'{pre}#{req}#{file.file_id}')] for file in files ] 
 
-    btn.insert(0, [InlineKeyboardButton("🌼 Jᴏɪɴ Fɪʟᴍ Cʜᴀɴɴᴇʟ 🌼", url="https://t.me/SilvaFilmZone")])
+    btn.insert(0, [InlineKeyboardButton("🌼 Jᴏɪɴ Fɪʟᴍ Cʜᴀɴɴᴇʟ 🌼", url="https://t.me/Sinhalasubmovies")])
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
         temp.GP_BUTTONS[key] = search
